ffee43afef5e83eec66849935ab03c9767f9d8a8
talescript <teichopsia@gmail.com>
2019-05-11 20:28:42 -0500

    Modified quickstart to match files on raspberry. Add notes on limits

--- quickstart.py
+++ quickstart.py
@@ -28,23 +28,25 @@ session = InstaPy(username=insta_username, password=insta_password,
 with smart_run(session):
     # settings first
     session.set_relationship_bounds(enabled=True, delimit_by_numbers=True,
-            max_followers=2000, min_followers=45, min_following=77)
+            max_followers=1600, min_followers=45, min_following=77)
 
-    session.set_quota_supervisor(enabled=True,
-            sleep_after=["likes", "follows"],
-            sleepyhead=True, stochastic_flow=True,
-            notify_me=False,
-            peak_likes=(100, 1000),
-            peak_comments=(21, 250),
-            peak_follows=(200, None))
+   # session.set_quota_supervisor(enabled=True,
+   #         sleep_after=["likes", "follows"],
+   #         sleepyhead=True, stochastic_flow=True,
+   #         notify_me=False,
+   #         peak_likes=(100, 1000),
+   #         peak_comments=(21, 250),
+   #         peak_follows=(200, None))
 
     # then actions
-    session.set_do_follow(enabled=True, percentage=45, times=2)
-    session.set_do_comment(enabled=True, percentage=85)
+    session.set_dont_unfollow_active_users(enabled=True, posts=6)
+    session.set_do_comment(enabled=True, percentage=55)
     session.set_comments(COMMENTS)
+    session.set_do_follow(enabled=True, percentage=65, times=2)
     # set_delimit_commenting has a bug that prevents comments from happening
 
-    session.like_by_tags(HASHTAGS, amount=7, use_smart_hashtags=False,
+    session.like_by_feed(amount=90, randomize=False)
+    session.like_by_tags(HASHTAGS, amount=20, use_smart_hashtags=False,
             skip_top_posts=True)
     session.unfollow_users(amount=45, InstapyFollowed=(True, "nonFollowers"), 
-            style="FIFO", unfollow_after=42*60*60, sleep_delay=450)
+            style="FIFO", unfollow_after=90*60*60, sleep_delay=450)
